782177a9a9c579539bb30a8ffab0988cb9285bcc
Gazee <100186961+Gazee1112@users.noreply.github.com>
2022-02-22 16:01:08 +0700

    Penambahan Inisialiasasi mahasiswa 3

--- Ngulik1.java
+++ Ngulik1.java
@@ -44,5 +44,21 @@ public class Main03 {
         System.out.println(mahasiswa2.jurusan);
         System.out.println(mahasiswa2.IPK);
         System.out.println(mahasiswa2.umur);
+        
+        Mahasiswa mahasiswa3 = new Mahasiswa();
+        
+        mahasiswa3.name = "Faza";
+        mahasiswa3.NIM = "2151500700111044";
+        mahasiswa3.jurusan = "Teknologi Informasi";
+        mahasiswa3.IPK = "3.69";
+        mahasiswa3.umur = "19";
+        
+        System.out.println(mahasiswa3.nama);
+        System.out.println(mahasiswa3.NIM);
+        System.out.println(mahasiswa3.jurusan);
+        System.out.println(mahasiswa3.IPK);
+        System.out.println(mahasiswa3.umur);
+        
+        
     }
 }    
