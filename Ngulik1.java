5c32f65440e060bbc8b03e9299e7dcb4f827f655
Gazee1112 <100186961+Gazee1112@users.noreply.github.com>
2022-02-22 15:03:00 +0700

    Pengenalan Class dan Object (Inisialisasi)

---
+++ Ngulik1.java
@@ -0,0 +1,48 @@
+package com.tutorial;
+
+import com.tutorial.Mahasiswa;
+
+// Pengenalan class dan object (Inisialisasi)
+// Membuat class sebagai template
+class Mahasiswa {
+    String nama;
+    String NIM;
+    String jurusan;
+    double IPK;
+    int umur;
+
+}
+
+public class Main03 {
+    public static void main(String[] args){
+        
+        // Inisiasi atau membuat Object
+        Mahasiswa mahasiswa1 = new Mahasiswa();
+        
+        mahasiswa1.nama = "Ajun";
+        mahasiswa1.NIM = "215150700111038";
+        mahasiswa1.jurusan = "Teknologi Informasi";
+        mahasiswa1.IPK = 3.85;
+        mahasiswa1.umur = 19;
+    
+        System.out.println(mahasiswa1.nama);
+        System.out.println(mahasiswa1.NIM);
+        System.out.println(mahasiswa1.jurusan);
+        System.out.println(mahasiswa1.IPK);
+        System.out.println(mahasiswa1.umur);
+
+        Mahasiswa mahasiswa2 = new Mahasiswa();
+        
+        mahasiswa2.nama = "Bariq";
+        mahasiswa2.NIM = "215150700111039";
+        mahasiswa2.jurusan = "Teknologi Informasi";
+        mahasiswa2.IPK = 3.70;
+        mahasiswa2.umur = 20;
+    
+        System.out.println(mahasiswa2.nama);
+        System.out.println(mahasiswa2.NIM);
+        System.out.println(mahasiswa2.jurusan);
+        System.out.println(mahasiswa2.IPK);
+        System.out.println(mahasiswa2.umur);
+    }
+}    
